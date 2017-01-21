c330e2f0afd359dcb98aa1cf8beead1fa029b569
Talie Chen <Talie@MacComputer1.local>
2017-01-21 15:45:19 -0800

    added names

--- sketch.js
+++ sketch.js
@@ -9,18 +9,23 @@ var c13x = 1500;
 var c13y = 600;
 var c13r = 400;
 
+var c12x = 400;
+var c12y = 600;
+var c12w = 1250;
+var c12h = 400;
 function setup() { 
   createCanvas(2000, 1300);
 } 
 
 function preload() {
-  img1 = loadImage("clickdebait1.png");
+  img1 = loadImage("dbbanner2.png");
   img2 = loadImage("speechbubble1.png");
   img3 = loadImage("speechbubble2.png");
+  img4 = loadImage("names.png");
 }
 
 function draw() { 
-  background(206, 187, 165);
+  background(244, 108, 66);
   
   image(img1, 260, -100, 1500, 1215);
   //image(img2, 260, -100, 1500, 1215);
@@ -47,6 +52,16 @@ function draw() {
 			  noFill();
 				stroke(0, 102, 153);
 				}
+  
+  
+  if (overRect(c12x, c12y, c12w, c12h)) {
+        
+         image(img4, 260, -100, 1500, 1215);
+			
+				}  else {
+			  noFill();
+				stroke(0, 102, 153);
+				}
 }
 
 
@@ -55,5 +70,13 @@ function overCircle(x, y, radius) {
 	  return true;	
 	} else {
 	  return false;	
+	}
+}
+
+function overRect(x, y, w, h) {
+	if (mouseX > x && mouseX < x + w && mouseY > y && mouseY < y + h) {
+		return true;
+	} else {
+		return false;
 	}
 }
